c91b6666a38a7f316ba55218447f205a5f4d0541
Neds001 <nedsmamitag001@gmail.com>
2023-06-26 08:49:44 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,32 +1,97 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-function App() {
-  const [gas, setGas] = useState([]);
+const App = () => {
+  const [gasData, setGasData] = useState(null);
 
-  const loadData = async () => {
+  const fetchData = async () => {
     const network = 'eth';
     const key = '3bb7a9ba8177423187079793b24b6748';
     const res = await fetch(`https://api.owlracle.info/v4/${network}/gas?apikey=${key}`);
     const data = await res.json();
-    setGas(data);
+    setGasData(data);
   };
 
   useEffect(() => {
-    loadData();
+    fetchData();
   }, []);
 
+  const reloadGasData = () => {
+    fetchData();
+  };
+
   return (
-    <View>
-      <FlatList
-        data={gas}
-        renderItem={(item)=> {
-          console.log(item);
-          return <Text>gas</Text>
-        }}
-      />
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.reloadButton} onPress={reloadGasData}>
+        <Text style={styles.reloadText}>Reload</Text>
+      </TouchableOpacity>
+      
+      {gasData && (
+        <View style={styles.gasDataContainer}>
+          <Text style={styles.headerText}>Gas Data:</Text>
+          <View style={styles.gasFeeContainer}>
+            <View style={styles.gasFeeItem}>
+              <Text style={styles.gasFeeLabel}>Low:</Text>
+              <Text style={styles.gasFeeValue}>{gasData.low}</Text>
+            </View>
+            <View style={styles.gasFeeItem}>
+              <Text style={styles.gasFeeLabel}>Average:</Text>
+              <Text style={styles.gasFeeValue}>{gasData.average}</Text>
+            </View>
+            <View style={styles.gasFeeItem}>
+              <Text style={styles.gasFeeLabel}>Priority:</Text>
+              <Text style={styles.gasFeeValue}>{gasData.priority}</Text>
+            </View>
+          </View>
+        </View>
+      )}
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  reloadButton: {
+    marginBottom: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: '#007AFF',
+    borderRadius: 8,
+  },
+  reloadText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  gasDataContainer: {
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  gasFeeContainer: {
+    alignItems: 'center',
+    marginTop: 16,
+  },
+  gasFeeItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  gasFeeLabel: {
+    fontSize: 16,
+    marginRight: 8,
+  },
+  gasFeeValue: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
